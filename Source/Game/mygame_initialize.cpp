b86509baa22708e1253411eb111298cca1befdc0
Kiri487 <kiri48787@gmail.com>
2023-03-03 18:10:22 +0800

    0.1

---
+++ Source/Game/mygame_initialize.cpp
@@ -0,0 +1,87 @@
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "mygame.h"
+
+using namespace game_framework;
+/////////////////////////////////////////////////////////////////////////////
+// 這個class為遊戲的遊戲開頭畫面物件
+/////////////////////////////////////////////////////////////////////////////
+
+CGameStateInit::CGameStateInit(CGame *g) : CGameState(g)
+{
+}
+
+void CGameStateInit::OnInit()
+{
+	//
+	// 當圖很多時，OnInit載入所有的圖要花很多時間。為避免玩遊戲的人
+	//     等的不耐煩，遊戲會出現「Loading ...」，顯示Loading的進度。
+	//
+	ShowInitProgress(0, "Loading ...");	// 一開始的loading進度為0%
+	Sleep(100);
+	
+	background.LoadBitmapByString({ "resources/initialize_background.bmp" });
+	background.SetTopLeft(0, 0);
+
+	title.LoadBitmapByString({ "resources/title1.bmp", "resources/title2.bmp" }, RGB(0, 0, 255));
+	title.SetTopLeft(631, 349);
+	title.SetAnimation(300, false);
+
+	//
+	// 此OnInit動作會接到CGameStaterRun::OnInit()，所以進度還沒到100%
+	//
+}
+
+void CGameStateInit::OnBeginState()
+{
+}
+
+void CGameStateInit::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	GotoGameState(GAME_STATE_RUN);		// 切換至GAME_STATE_RUN
+}
+
+void CGameStateInit::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	GotoGameState(GAME_STATE_RUN);		// 切換至GAME_STATE_RUN
+}
+
+void CGameStateInit::OnShow()
+{
+	background.ShowBitmap();
+	title.ShowBitmap();
+	draw_text();
+}
+
+void CGameStateInit::draw_text() {
+
+	CDC *pDC = CDDraw::GetBackCDC();
+
+	CTextDraw::ChangeFontLog(pDC, 15, "Press Start 2P", RGB(0, 0, 0));
+	text_border(pDC, 545,632, 4, "Press any key to start");
+
+	CTextDraw::ChangeFontLog(pDC, 15, "Press Start 2P", RGB(255, 255, 255));
+	CTextDraw::Print(pDC, 545, 632, "Press any key to start");
+
+	CTextDraw::Print(pDC, 15, 695, "By NTUT-Kiri, NTUT-Shuan");
+
+	CDDraw::ReleaseBackCDC();
+}
+
+void CGameStateInit::text_border(CDC *pDC, int x, int y, int size, string s) {
+	for (int i = 0; i < size; i++) {
+		CTextDraw::Print(pDC, x, y + i, s);
+		CTextDraw::Print(pDC, x, y - i, s);
+		CTextDraw::Print(pDC, x + i, y, s);
+		CTextDraw::Print(pDC, x - i, y, s);
+		CTextDraw::Print(pDC, x + i, y + i, s);
+		CTextDraw::Print(pDC, x + i, y - i, s);
+		CTextDraw::Print(pDC, x - i, y + i, s);
+		CTextDraw::Print(pDC, x - i, y - i, s);
+	}
+}
---
+++ Source/Game/mygame_run.cpp
@@ -0,0 +1,164 @@
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "mygame.h"
+
+using namespace game_framework;
+
+/////////////////////////////////////////////////////////////////////////////
+// 這個class為遊戲的遊戲執行物件，主要的遊戲程式都在這裡
+/////////////////////////////////////////////////////////////////////////////
+
+CGameStateRun::CGameStateRun(CGame *g) : CGameState(g)
+{
+}
+
+CGameStateRun::~CGameStateRun()
+{
+}
+
+void CGameStateRun::OnBeginState()
+{
+}
+
+void CGameStateRun::OnMove()							// 移動遊戲元素
+{
+
+	
+	
+}
+
+void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
+{
+	background.LoadBitmapByString({
+	"resources/1_background.bmp",
+	"resources/2_background.bmp",
+	"resources/3_background.bmp",
+	"resources/4_background.bmp",
+	"resources/5_background.bmp",
+		});
+	background.SetTopLeft(0, 0);
+
+	map.LoadBitmapByString({
+	"resources/1_map.bmp",
+	"resources/2_map.bmp"
+		}, RGB(0, 0, 255));
+
+	music.LoadBitmapByString({ "resources/music_on.bmp", "resources/music_off.bmp" });
+	music.SetTopLeft(1369, 25);
+
+	sound.LoadBitmapByString({ "resources/sound_on.bmp", "resources/sound_off.bmp" });
+	sound.SetTopLeft(1445, 25);
+
+	exit.LoadBitmapByString({ "resources/exit.bmp" });
+	exit.SetTopLeft(25, 25);
+
+	character.LoadBitmapByString({ "resources/bob1.bmp" }, RGB(0, 0, 255));
+
+	box1.LoadBitmapByString({ "resources/1_box.bmp" });
+	box1.SetTopLeft(802, 228);
+
+	goal1.LoadBitmapByString({ "resources/1_goal.bmp" }, RGB(0, 0, 255));
+	goal1.SetTopLeft(802, 471);
+
+}
+
+void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	
+}
+
+void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	level++;
+}
+
+void CGameStateRun::OnLButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnLButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnMouseMove(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnRButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnShow()
+{
+	show_image_by_level();
+	show_text_by_level();
+}
+
+void CGameStateRun::show_image_by_level() {
+	
+	if (level <= 6) {
+		background.SetFrameIndexOfBitmap(level - 1);
+		background.ShowBitmap();
+		map.SetFrameIndexOfBitmap(level - 1);
+		map.ShowBitmap();
+		character.ShowBitmap();
+		music.ShowBitmap();
+		sound.ShowBitmap();
+		exit.ShowBitmap();
+		
+		if (level == 1) {
+			map.SetTopLeft(487, 136);
+			character.SetTopLeft(625, 207);
+			box1.ShowBitmap();
+			goal1.ShowBitmap();
+		}
+
+		if (level == 2) {
+			map.SetTopLeft(501, 233);
+			character.SetTopLeft(522, 330);
+		}
+	}
+}
+
+void CGameStateRun::show_text_by_level() {
+
+	CDC *pDC = CDDraw::GetBackCDC();
+	CTextDraw::ChangeFontLog(pDC, 15, "Press Start 2P", RGB(255, 255, 255));
+
+	if (level == 1) {
+		CTextDraw::ChangeFontLog(pDC, 15, "Press Start 2P", RGB(0, 0, 0));
+		text_border(pDC, 15, 695, 4, "Level 1");
+		CTextDraw::ChangeFontLog(pDC, 15, "Press Start 2P", RGB(255, 255, 255));
+		CTextDraw::Print(pDC, 15, 695, "Level 1");
+	}
+	else if (level == 2) {
+		CTextDraw::ChangeFontLog(pDC, 15, "Press Start 2P", RGB(0, 0, 0));
+		text_border(pDC, 15, 695, 4, "Level 2");
+		CTextDraw::ChangeFontLog(pDC, 15, "Press Start 2P", RGB(255, 255, 255));
+		CTextDraw::Print(pDC, 15, 695, "Level 2");
+	}
+
+	CDDraw::ReleaseBackCDC();
+}
+
+void CGameStateRun::text_border(CDC *pDC, int x, int y, int size, string s) {
+	for (int i = 0; i < size; i++) {
+		CTextDraw::Print(pDC, x, y + i, s);
+		CTextDraw::Print(pDC, x, y - i, s);
+		CTextDraw::Print(pDC, x + i, y, s);
+		CTextDraw::Print(pDC, x - i, y, s);
+		CTextDraw::Print(pDC, x + i, y + i, s);
+		CTextDraw::Print(pDC, x + i, y - i, s);
+		CTextDraw::Print(pDC, x - i, y + i, s);
+		CTextDraw::Print(pDC, x - i, y - i, s);
+	}
+}
