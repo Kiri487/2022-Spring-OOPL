482e128a6f6fd0f99f8d90819340c67653aed1f6
Shuan0402 <st980155@gmail.com>
2023-04-03 17:49:55 +0800

    object.h

--- Source/Game/object.h
+++ Source/Game/object.h
@@ -17,6 +17,9 @@ private:
 	ObjectType objecttybe;
 	game_framework::CMovingBitmap image;
 	game_framework::CMovingBitmap charater;
+	game_framework::CMovingBitmap sbox;
+	game_framework::CMovingBitmap mbox;
+	game_framework::CMovingBitmap lbox;
 
 public:
 	void ShowObjectImage();
